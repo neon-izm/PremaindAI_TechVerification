bfb7652dfc6c8a5e7d0cdf0e1e16836911edcd5a
Kirurobo <kirurobo@gmail.com>
2019-07-14 03:55:40 +0900

    モデルがnormalizeされていなくても可動とした。元のモデルのリポジトリを基にモデルのプレファブ再作成。

--- MotionDataVerification/Assets/_PremaidAI/ModelData/ModelJoint.cs
+++ MotionDataVerification/Assets/_PremaidAI/ModelData/ModelJoint.cs
@@ -16,71 +16,78 @@ namespace PreMaid
     {
         [Header("02とか1Cとか当てる")]
         public string ServoID;
-        
+
         public enum Axis
         {
-            X,Y,Z
+            X, Y, Z
         }
 
         [SerializeField] private bool isInverse = false;
         //もしかして取り付け軸向きのinverseもenum定義した方がいいかも？
-        
-        [SerializeField] private Axis targetAxis= Axis.X;
-        
-        Vector3 initRotation= Vector3.zero;
-        
+
+        [SerializeField] private Axis targetAxis = Axis.X;
+
+        // 初期ローカル姿勢
+        Quaternion initialLocalRotation = Quaternion.identity;
+
+        // ローカル座標系でのサーボ回転軸
+        Vector3 localServoAxis = Vector3.forward;
+
         // Start is called before the first frame update
         void Start()
         {
-            initRotation = transform.localEulerAngles;
+            initialLocalRotation = transform.localRotation;
+
+            // サーボ回転軸を求めておく
+            Vector3 axis;
+            switch (targetAxis)
+            {
+                case Axis.X:
+                    axis = Vector3.right;
+                    break;
+                case Axis.Y:
+                    axis = Vector3.up;
+                    break;
+                case Axis.Z:
+                    axis = Vector3.forward;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            if (isInverse)
+            {
+                axis *= -1f;
+            }
+
+            // 元のローカル姿勢が正規化されていなくても対応するため、ルートの姿勢を基準にする
+            Transform rootTransform = GetModelRootTransform();
+            localServoAxis = Quaternion.Inverse(transform.rotation) * (rootTransform.rotation * axis);
         }
 
         /// <summary>
-        /// 外部から指定する
+        /// モデルのルートには Animator があると仮定して、ルートのTransformを探す
+        /// </summary>
+        /// <returns></returns>
+        private Transform GetModelRootTransform()
+        {
+            Transform t = transform;
+            while (true)
+            {
+                if (t.GetComponent<Animator>()) break;  // Animator があればモデルのルートとみなす
+                if (!t.parent) break;   // これ以上親がなければ、ルートとみなす
+                t = t.parent;
+            }
+            return t;
+        }
+
+        /// <summary>
+        /// 外部からサーボ角度[deg]を指定する
         /// </summary>
         /// <param name="angleEulerDegree"></param>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         public void SetServoValue(float angleEulerDegree)
         {
-            Vector3 targetLocalRotation = initRotation;
-            switch (targetAxis)
-            {
-                case Axis.X:
-                    if (isInverse)
-                    {
-                        targetLocalRotation.x += -angleEulerDegree;
-                    }
-                    else
-                    {
-                        targetLocalRotation.x += angleEulerDegree;
-                    }
-
-                    break;
-                case Axis.Y:
-                    if (isInverse)
-                    {
-                        targetLocalRotation.y += -angleEulerDegree;
-                    }
-                    else
-                    {
-                        targetLocalRotation.y += angleEulerDegree;
-                    }
-                    break;
-                case Axis.Z:
-                    if (isInverse)
-                    {
-                        targetLocalRotation.z += -angleEulerDegree;
-                    }
-                    else
-                    {
-                        targetLocalRotation.z += angleEulerDegree;
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            
-            transform.localRotation= Quaternion.Euler(targetLocalRotation);
+            transform.localRotation = initialLocalRotation * Quaternion.AngleAxis(angleEulerDegree, localServoAxis);
         }
     }
 }
