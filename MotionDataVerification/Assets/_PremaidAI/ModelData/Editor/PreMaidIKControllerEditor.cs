7e2c3d167e0f32ec1d3789b8067b78e3790979b3
Kirurobo <kirurobo@gmail.com>
2019-07-15 08:14:16 +0900

    手先優先IKでは一旦上腕ピッチは除外。

---
+++ MotionDataVerification/Assets/_PremaidAI/ModelData/Editor/PreMaidIKControllerEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace PreMaid
+{
+    [CustomEditor(typeof(PreMaidIKController))]
+    public class PreMaidIKControllerEditor : Editor
+    {
+        private void OnSceneGUI()
+        {
+            PreMaidIKController controller = (PreMaidIKController)target;
+
+
+            if (controller.priorJoint == PreMaidIKController.Arm.PriorJoint.Elbow)
+            {
+                if (!controller.leftHandTarget || !controller.rightHandTarget || !controller.leftElbowTarget || !controller.rightElbowTarget) return;
+
+                EditorGUI.BeginChangeCheck();
+
+                Vector3 leftWristPos = Handles.PositionHandle(controller.leftHandTarget.position, Quaternion.identity);
+                Vector3 rightWristPos = Handles.PositionHandle(controller.rightHandTarget.position, Quaternion.identity);
+                Vector3 leftElbowPos = Handles.PositionHandle(controller.leftElbowTarget.position, Quaternion.identity);
+                Vector3 rightElbowPos = Handles.PositionHandle(controller.rightElbowTarget.position, Quaternion.identity);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    controller.leftHandTarget.position = leftWristPos;
+                    controller.rightHandTarget.position = rightWristPos;
+                    controller.leftElbowTarget.position = leftElbowPos;
+                    controller.rightElbowTarget.position = rightElbowPos;
+                }
+            }
+            else
+            {
+                if (!controller.leftHandTarget || !controller.rightHandTarget) return;
+
+                EditorGUI.BeginChangeCheck();
+                Vector3 leftWristPos = Handles.PositionHandle(controller.leftHandTarget.position, Quaternion.identity);
+                Vector3 rightWristPos = Handles.PositionHandle(controller.rightHandTarget.position, Quaternion.identity);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    controller.leftHandTarget.position = leftWristPos;
+                    controller.rightHandTarget.position = rightWristPos;
+                }
+            }
+        }
+    }
+}
--- MotionDataVerification/Assets/_PremaidAI/ModelData/PreMaidIKController.cs
+++ MotionDataVerification/Assets/_PremaidAI/ModelData/PreMaidIKController.cs
@@ -6,8 +6,6 @@ namespace PreMaid
 {
     public class PreMaidIKController : MonoBehaviour
     {
-        [SerializeField] private Transform premaidRoot;
-
         private ModelJoint[] _joints;
 
 
@@ -19,9 +17,10 @@ namespace PreMaid
             public ModelJoint upperArmPitch;
             public ModelJoint lowerArmRoll;
             public ModelJoint handPitch;
+            private Transform handTip;
 
             public Transform elbowTarget;
-            public Transform wristTarget;
+            public Transform handTarget;
             public float handAngle;
 
             /// <summary>
@@ -35,7 +34,7 @@ namespace PreMaid
             {
                 None,
                 Elbow,
-                Wrist
+                Hand
             }
 
             float lengthShoulder;
@@ -44,11 +43,20 @@ namespace PreMaid
 
             public void Initialize()
             {
+                baseTransform = shoulderPitch.transform.parent;
+                if (handPitch.transform.childCount < 1)
+                {
+                    handTip = handPitch.transform;  // 子が無かった場合はhandPitchを手先とする
+                }
+                else
+                {
+                    handTip = handPitch.transform.GetChild(0);  // 子があればそれを手先とする
+                }
+
                 lengthShoulder = (upperArmRoll.transform.position - shoulderPitch.transform.position).magnitude;
                 lengthUpperArm = (lowerArmRoll.transform.position - upperArmRoll.transform.position).magnitude;
-                lengthLowerArm = (handPitch.transform.position - lowerArmRoll.transform.position).magnitude;
+                lengthLowerArm = (handTip.position - lowerArmRoll.transform.position).magnitude;
 
-                baseTransform = shoulderPitch.transform.parent;
                 
                 // 肘の目標点が無ければ自動生成
                 if (!elbowTarget)
@@ -60,78 +68,167 @@ namespace PreMaid
                 }
 
                 // 手首の目標点が無ければ自動生成
-                if (!wristTarget)
+                if (!handTarget)
                 {
-                    var obj = new GameObject((isRightSide ? "Right" : "Left") + "WristTarget");
-                    wristTarget = obj.transform;
-                    wristTarget.parent = baseTransform;
-                    wristTarget.position = handPitch.transform.position;
+                    var obj = new GameObject((isRightSide ? "Right" : "Left") + "HandTarget");
+                    handTarget = obj.transform;
+                    handTarget.parent = baseTransform;
+                    handTarget.position = handPitch.transform.position;
                 }
             }
 
             public void ApplyIK()
             {
-                if (!elbowTarget || !wristTarget) return;
+                if (!elbowTarget || !handTarget) return;
 
-                if (priorJoint == PriorJoint.Elbow)
+                switch (priorJoint)
                 {
-                    ApplyIK_ElbowFirst();
-                }
-                else
-                {
-                    ApplyIK_WristFirst();
+                    case PriorJoint.Elbow:
+                        ApplyIK_ElbowFirst();
+                        break;
+                    case PriorJoint.Hand:
+                        ApplyIK_HandFirst();
+                        break;
                 }
             }
 
+            /// <summary>
+            /// 肘座標優先モードで腕のIKを解き、順次サーボ角度を設定していく
+            /// </summary>
             private void ApplyIK_ElbowFirst()
             {
                 // これ以下に肩に近づきすぎた肘目標点は無視する閾値
                 const float sqrMinDistance = 0.0001f;   // [m^2]
 
-                if ((elbowTarget.position - shoulderPitch.transform.position).sqrMagnitude < sqrMinDistance)
+                float sign = (isRightSide ? -1f : 1f);  // 左右の腕による方向入れ替え用
+
+                Vector3 x0 = shoulderPitch.transform.position;  // UpperArmJointの座標
+                Quaternion invBaseRotation = Quaternion.Inverse(baseTransform.rotation);
+                Vector3 x0e = invBaseRotation * (elbowTarget.position - x0);    // x0から肘目標点までのベクトル
+
+                // 閾値より肘目標点が肩に近すぎる場合、手首優先モードのIKにする
+                if (x0e.sqrMagnitude < sqrMinDistance)
                 {
-                    ApplyIK_WristFirst();
+                    ApplyIK_HandFirst();
                     return;
                 }
-
-                float sign = (isRightSide ? -1f : 1f);
-
-                Quaternion invBaseRotation = Quaternion.Inverse(baseTransform.rotation);
-                //Debug.Log(invBaseRotation.eulerAngles);
-
-                Vector3 x0 = shoulderPitch.transform.position;
-                Vector3 x0e = invBaseRotation * (elbowTarget.position - x0);
 
                 float a0 = Mathf.Atan2(sign * x0e.z, -x0e.y) * Mathf.Rad2Deg; // 肩のX軸周り回転[deg]
                 shoulderPitch.SetServoValue(a0);
 
-                Vector3 x1 = upperArmRoll.transform.position;
+                Vector3 x1 = upperArmRoll.transform.position;   // 上腕始点の座標
                 Quaternion invShoulderRotation = Quaternion.Inverse(shoulderPitch.normalizedRotation);
                 Vector3 x1e = invShoulderRotation * (elbowTarget.position - x1);
                 float a1 = Mathf.Atan2(sign * x1e.y, sign * -x1e.x) * Mathf.Rad2Deg; // 上腕のZ軸周り回転[deg]
                 upperArmRoll.SetServoValue(a1);
 
-                Vector3 x3 = lowerArmRoll.transform.position;
+                Vector3 x3 = lowerArmRoll.transform.position;   // 肘座標
                 Quaternion invUpperArmRotation = Quaternion.Inverse(upperArmRoll.normalizedRotation);
-                Vector3 x3w = invUpperArmRotation * (wristTarget.position - x3);
+                Vector3 x3h = invUpperArmRotation * (handTarget.position - x3);    // 肘から手首目標点へ向かうベクトル
 
-                if (x3w.sqrMagnitude < sqrMinDistance)
+                // 閾値より手首目標点が肘に近すぎる場合は、肘から先は処理しない
+                if (x3h.sqrMagnitude < sqrMinDistance)
                 {
                     return;
                 }
 
-                float a2 = Mathf.Atan2(-x3w.x, -x3w.z) * Mathf.Rad2Deg; // 上腕のX軸周り回転[deg]
+                float a2 = Mathf.Atan2(-x3h.x, -x3h.z) * Mathf.Rad2Deg; // 上腕のX軸周り回転[deg]
                 upperArmPitch.SetServoValue(a2);
 
                 Vector3 x4 = handPitch.transform.position;
-                float l3w = Mathf.Sqrt(x3w.z * x3w.z + x3w.x * x3w.x);
-                float a3 = Mathf.Atan2(sign * -l3w, x3w.y) * Mathf.Rad2Deg; // 前腕のZ軸周り回転[deg]
+                float l3h = Mathf.Sqrt(x3h.z * x3h.z + x3h.x * x3h.x);
+                float a3 = Mathf.Atan2(sign * -l3h, x3h.y) * Mathf.Rad2Deg; // 前腕のZ軸周り回転[deg]
                 lowerArmRoll.SetServoValue(a3);
             }
 
-            private void ApplyIK_WristFirst()
+            /// <summary>
+            /// 手先座標優先モードで腕のIKを解き、順次サーボ角度を設定していく
+            /// </summary>
+            private void ApplyIK_HandFirst()
             {
+                // これ以下に肩に近づきすぎた肘目標点は無視する閾値
+                const float sqrMinDistance = 0.000025f;   // [m^2]
 
+                float sign = (isRightSide ? -1f : 1f);  // 左右の腕による方向入れ替え用
+
+                Vector3 x0 = shoulderPitch.transform.position;  // UpperArmJointの座標
+                Quaternion invBaseRotation = Quaternion.Inverse(baseTransform.rotation);
+                Vector3 x0h = invBaseRotation * (handTarget.position - x0);    // x0から肘目標点までのベクトル
+
+                if ((x0h.y * x0h.y + x0h.z * x0h.z) < sqrMinDistance)
+                {
+                    // 肩ピッチの特異点近傍であれば、回転させない
+                }
+                else
+                {
+                    // 特異点近傍でなければ、回転させる
+                    float a0 = sign * Mathf.Atan2(x0h.z, -x0h.y) * Mathf.Rad2Deg; // 肩のX軸周り回転[deg]
+                    shoulderPitch.SetServoValue(a0);
+                }
+
+                Vector3 x1 = upperArmRoll.transform.position;
+                Quaternion invShoulderRotation = Quaternion.Inverse(shoulderPitch.normalizedRotation);
+                Vector3 x1h = invShoulderRotation * (handTarget.position - x1);
+
+                float a1 = 0f;
+                float a2 = 0f;
+                float a3 = 0f;
+
+                float x1w_sqrlen = x1h.sqrMagnitude;
+
+                if (x1w_sqrlen <= (lengthUpperArm - lengthLowerArm) * (lengthUpperArm - lengthLowerArm))
+                {
+                    // 上腕始点に手首目標点が近すぎて三角形にならない場合
+                    a3 = sign * -135f;
+                    a1 = sign * 0f;
+                }
+                else if (x1w_sqrlen >= (lengthUpperArm + lengthLowerArm) * (lengthUpperArm + lengthLowerArm))
+                {
+                    // 腕を伸ばした以上に手首目標点が遠くて三角形にならない場合
+                    a3 = sign * 10f;   // 手を伸ばすときには、あえて過伸展
+                    a1 = sign * Mathf.Atan2(x1h.y, sign * -x1h.x) * Mathf.Rad2Deg - a3;
+                }
+                else
+                {
+                    // 三角形になる場合、余弦定理により肘の角度を求める
+                    float cosx3 = (lengthUpperArm * lengthUpperArm + lengthLowerArm * lengthLowerArm - x1w_sqrlen) / (2f * lengthUpperArm * lengthLowerArm);
+                    a3 = sign * (Mathf.Acos(cosx3) * Mathf.Rad2Deg - 180f);
+                    float cosa1d = (lengthUpperArm * lengthUpperArm + x1w_sqrlen - lengthLowerArm * lengthLowerArm) / (2f * lengthUpperArm * Mathf.Sqrt(x1w_sqrlen));
+                    float a1sub = Mathf.Acos(cosa1d) * Mathf.Rad2Deg;
+                    a1 = sign * (Mathf.Atan2(x1h.y, sign * -x1h.x) * Mathf.Rad2Deg + a1sub);
+                }
+                upperArmRoll.SetServoValue(a1);
+                upperArmPitch.SetServoValue(a2);
+                lowerArmRoll.SetServoValue(a3);
+
+                //// 関節0に可動限界があるため、関節1を動かした後に関節2の回転で補う
+                //x1 = upperArmRoll.transform.position;
+                //Quaternion invUpperArmRotation = Quaternion.Inverse(upperArmRoll.normalizedRotation);
+
+                //x1w = (invUpperArmRotation * (handTarget.position - x1));
+                //Vector3 x13 = (invUpperArmRotation * (lowerArmRoll.transform.position - x1));
+                //Vector3 x15 = (invUpperArmRotation * (handTip.position - x1));
+
+                //float x13_sqrlen = x13.sqrMagnitude;
+                //Vector3 x5n = (x15 - Vector3.Dot(x13, x15) / x13_sqrlen * x13).normalized;   // 今の手先の点に向かうx13の単位法線ベクトル
+                //Vector3 xwn = (x1w - Vector3.Dot(x13, x1w) / x13_sqrlen * x13).normalized;   // 手先目標点に向かうx13の単位法線ベクトル
+
+                //float cos5wn = Vector3.Dot(x5n, xwn);
+                //if (cos5wn >= 0.98f)
+                //{
+                //}
+                //else
+                //{
+                //    a2 = sign * (Mathf.Acos(cos5wn) * Mathf.Rad2Deg);
+                //    if (a2 < 180f) a2 += 180f;
+                //    if (a2 > 180f) a2 -= 180f;
+                //    //if (float.IsNaN(a2)) Debug.Log("cos:" + cos5wn);
+                //    //if (!isRightSide) Debug.Log(a2);
+
+                //    upperArmPitch.SetServoValue(a2);
+                //}
+
+                
             }
 
             public void DrawGizmos()
@@ -141,16 +238,12 @@ namespace PreMaid
                 if (priorJoint == Arm.PriorJoint.Elbow)
                 {
                     Gizmos.color = Color.red;
-                }
-                else
-                {
-                    Gizmos.color = Color.yellow;
-                }
 
-                if (elbowTarget)
-                {
-                    Gizmos.DrawLine(lowerArmRoll.transform.position, elbowTarget.position);
-                    Gizmos.DrawSphere(elbowTarget.position, gizmoRadius);
+                    if (elbowTarget)
+                    {
+                        Gizmos.DrawLine(lowerArmRoll.transform.position, elbowTarget.position);
+                        Gizmos.DrawSphere(elbowTarget.position, gizmoRadius);
+                    }
                 }
 
                 if (priorJoint == Arm.PriorJoint.Elbow)
@@ -162,32 +255,35 @@ namespace PreMaid
                     Gizmos.color = Color.red;
                 }
 
-                if (wristTarget)
+                if (handTarget)
                 {
-                    Gizmos.DrawLine(handPitch.transform.position, wristTarget.position);
-                    Gizmos.DrawSphere(wristTarget.position, gizmoRadius);
+                    Gizmos.DrawLine(handPitch.transform.position, handTarget.position);
+                    Gizmos.DrawSphere(handTarget.position, gizmoRadius);
                 }
 
             }
         }
 
 
-        private enum JointName
-        {
-            ShoulderPitch,
-            ShoulderRoll,
+        [Tooltip("ロボットモデルです。それ自体にアタッチされていれば未指定で構いません")]
+        public Transform premaidRoot;
 
+        [Tooltip("左手先目標です。未指定ならば自動生成します")]
+        public Transform leftHandTarget;
 
-        }
+        [Tooltip("右手先目標です。未指定ならば自動生成します")]
+        public Transform rightHandTarget;
 
+        [Tooltip("左手肘目標です。未指定ならば自動生成します")]
         public Transform leftElbowTarget;
+
+        [Tooltip("右手肘目標です。未指定ならば自動生成します")]
         public Transform rightElbowTarget;
-        public Transform leftWristTarget;
-        public Transform rightWristTarget;
 
         private Arm leftArm;
         private Arm rightArm;
 
+        [Tooltip("IKの基準を何に置くかです")]
         public Arm.PriorJoint priorJoint = Arm.PriorJoint.Elbow;
 
         // Start is called before the first frame update
@@ -211,10 +307,12 @@ namespace PreMaid
             leftArm.upperArmPitch = GetJointById("0F");
             leftArm.lowerArmRoll = GetJointById("13");
             leftArm.handPitch = GetJointById("17");
+            leftArm.handTarget = leftHandTarget;
+            leftArm.elbowTarget = leftElbowTarget;
             leftArm.Initialize();
 
             leftElbowTarget = leftArm.elbowTarget;
-            leftWristTarget = leftArm.wristTarget;
+            leftHandTarget = leftArm.handTarget;
 
             rightArm = new Arm();
             rightArm.isRightSide = true;
@@ -223,10 +321,12 @@ namespace PreMaid
             rightArm.upperArmPitch = GetJointById("0D");
             rightArm.lowerArmRoll = GetJointById("11");
             rightArm.handPitch = GetJointById("15");
+            rightArm.handTarget = rightHandTarget;
+            rightArm.elbowTarget = rightElbowTarget;
             rightArm.Initialize();
 
             rightElbowTarget = rightArm.elbowTarget;
-            rightWristTarget = rightArm.wristTarget;
+            rightHandTarget = rightArm.handTarget;
 
         }
 
